3f13ac1cfe482c7d9a89e699dae2461ba30939fc
Elia Peruzzo <tp_elia.peruzzo@sas.com>
2020-05-27 10:22:36 +0200

    Remove theme files

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Nuovo Post",
+        "excerpt":"\\[x + y = z\\] ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/helia95/nuovo-post/",
+        "teaser": null
+      }]
