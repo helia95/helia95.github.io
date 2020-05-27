7704f64cbcfa5f6e47670a6e9cc25a20ef5ff8ec
Elia Peruzzo <tp_elia.peruzzo@sas.com>
2020-05-27 18:17:58 +0200

    first commit

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Nuovo Post",
-        "excerpt":"\\[x + y = z\\] ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/helia95/nuovo-post/",
-        "teaser": null
-      }]
